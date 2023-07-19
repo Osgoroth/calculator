2e055b5243a12772272ad54d33d0c58be52198a4
David <46662617+Osgoroth@users.noreply.github.com>
2023-07-19 13:40:22 +0100

    button layout done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+
+const calculatorButtons = [
+  { id: "clear", value: "AC" },
+  { id: "divide", value: "/" },
+  { id: "multiply", value: "*" },
+  { id: "seven", value: 7 },
+  { id: "eight", value: 8 },
+  { id: "nine", value: 9 },
+  { id: "minus", value: "-" },
+  { id: "four", value: 4 },
+  { id: "five", value: 5 },
+  { id: "six", value: 6 },
+  { id: "plus", value: "+" },
+  { id: "one", value: 1 },
+  { id: "two", value: 2 },
+  { id: "three", value: 3 },
+  { id: "zero", value: 0 },
+  { id: "decimal", value: "." },
+  { id: "equals", value: "=" },
+];
 
 function App() {
+  const [input, setInput] = useState(0);
+  const [output, setOutput] = useState();
+  function handleClick(e) {
+    setOutput(output + e.target.value);
+    setInput(e.target.value);
+  }
+
+  function handleSum {
+    setOutput
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-sm align--center">
+      <div id="calculator" className="bg-primary">
+        <div className="output">{output}</div>
+        <div id="display" className="input">
+          {input}
+        </div>
+
+        <div id="buttons">
+          {calculatorButtons.map(({ id, value }) => {
+            return (
+              <button id={id} value={value} onClick={id === "equals" ? handleSum : handleClick}>
+                {value}
+              </button>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
